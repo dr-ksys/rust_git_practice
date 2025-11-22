76d6b0be61103b5781b0dcd5601eab7ae8693b92
Konstantin Sultanbaev <70580740+dr-ksys@users.noreply.github.com>
2025-11-22 19:47:23 +0300

    Delete rust_hack_intro/target directory

--- rust_hack_intro/target/debug/rust_hack_intro.d
+++
@@ -1,1 +0,0 @@
-/home/ks/vgit/rust_git_practice/rust_hack_intro/target/debug/rust_hack_intro: /home/ks/vgit/rust_git_practice/rust_hack_intro/src/main.rs
--- rust_hack_intro/target/release/rust_hack_intro.d
+++
@@ -1,1 +0,0 @@
-/home/ks/vgit/rust_git_practice/rust_hack_intro/target/release/rust_hack_intro: /home/ks/vgit/rust_git_practice/rust_hack_intro/src/main.rs
